2bfbf6104290cfc4eca7cffbe86cf1e4fad63d9a
Patryk <Patryk@PATRYK-PC>
2016-10-20 12:36:49 +0200

    Add project files.

---
+++ cwiczenia1910/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication14
+{
+    class Car   //zad1
+    {
+        private double pojemnoscSilnika; //zad2
+        private string marka;
+        static int iloscKol;
+
+        public Car()    //zad4
+        {
+            pojemnoscSilnika = 2.3;
+            marka = "blabla";
+        }
+        public Car(double pojemnoscSilnika, string marka)   //zad5
+        {
+            this.pojemnoscSilnika = pojemnoscSilnika;
+            this.marka = marka;
+
+        }
+        static public Car Create()  //zad9
+        {
+            return new Car();
+        }
+
+        static Car()
+        {
+            iloscKol = 4;
+        }
+
+        ~Car()
+        {
+            Console.WriteLine("Zwalniam pamiec");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ cwiczenia1910/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car();   //zad3
+            Car car2 = new Car(2.4, "Audi");    //zad6
+
+            Car car3 = Car.Create();    //zad10
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
